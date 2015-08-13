71362e89ab333e40d188447504d00c3e16ef3d22
Luís Alves <luis.alves@csc.fi>
2015-08-13 14:46:30 +0300

    added role for provisioning VMs on admin node

---
+++ roles/provision_vm/templates/virt-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ ! -f /etc/libvirt/qemu/{{ inventory_hostname }}.xml ]; then
+
+virt-install \
+  --name={{ inventory_hostname }} \
+  --os-variant=rhel7 \
+  --cpu host \
+  --vcpus={{ vcpus }} \
+  --ram={{ ram }} \
+{% for disk in disks %}
+  --disk path={{ disk.path }},cache=writethrough,device=disk,bus=virtio,size={{ disk.size }},format=raw \
+{% endfor %}
+{% for bridge in bridges %}
+  --network bridge={{ bridge }},model=virtio \
+{% endfor %}
+  --connect=qemu:///system \
+  --location={{ location }} \
+  --graphics=vnc \
+  --noautoconsole \
+  --wait=10 \
+  --initrd-inject={{ kickstart_tempdir }}/{{ inventory_hostname }}.ks \
+  --extra-args="ks=file:/{{ inventory_hostname }}.ks"
+fi
