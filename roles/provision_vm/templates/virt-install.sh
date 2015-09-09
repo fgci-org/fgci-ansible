2d4b5b2f2bdfb3f4eeecbfdc3eedd62c73414bfb
Luís Alves <luis.alves@csc.fi>
2015-09-09 11:48:08 +0300

    small changes on provision_vm role

--- roles/provision_vm/templates/virt-install.sh
+++ roles/provision_vm/templates/virt-install.sh
@@ -3,22 +3,23 @@
 if [ ! -f /etc/libvirt/qemu/{{ inventory_hostname }}.xml ]; then
 
 virt-install \
-  --name={{ inventory_hostname }} \
-  --os-variant=rhel7 \
+  --name {{ inventory_hostname }} \
+  --os-variant rhel7 \
   --cpu host-model \
-  --vcpus={{ vcpus }} \
-  --ram={{ ram }} \
+  --vcpus {{ vcpus }} \
+  --ram {{ ram }} \
 {% for disk in disks %}
   --disk path={{ disk.path }},cache=writethrough,device=disk,bus=virtio,size={{ disk.size }},format=raw \
 {% endfor %}
 {% for bridge in bridges %}
   --network bridge={{ bridge }},model=virtio \
 {% endfor %}
-  --connect=qemu:///system \
-  --location={{ location }} \
-  --graphics=vnc \
+  --connect qemu:///system \
+  --location '{{ location }}' \
+  --graphics vnc \
   --noautoconsole \
-  --wait=10 \
-  --initrd-inject={{ kickstart_tempdir }}/{{ inventory_hostname }}.ks \
-  --extra-args="ks=file:/{{ inventory_hostname }}.ks"
+  --wait 10 \
+  --initrd-inject '{{ kickstart_tempdir }}/{{ inventory_hostname }}.ks' \
+  --extra-args 'ks=file:/{{ inventory_hostname }}.ks console=ttyS0,115200n8' \
+  --console pty,target_type=serial
 fi
