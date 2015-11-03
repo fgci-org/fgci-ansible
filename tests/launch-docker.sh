2b2031f9d398809ffbb169e6eaa8941604f440af
Johan Guldmyr <johan.guldmyr@csc.fi>
2015-11-03 14:54:14 +0200

    rw=true in docker run command

--- tests/launch-docker.sh
+++ tests/launch-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker build --rm -t local/c7-systemd -f tests/Dockerfile .
-sudo docker run -it --privileged=true --rm=false -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v `pwd`:/fgci-ansible:rw local/c7-systemd /bin/bash -c "/fgci-ansible/tests/test-in-docker-image.sh ${OS_TYPE} ${OS_VERSION} ${ANSIBLE_VERSION}"
+sudo docker run -it --privileged=true --rm=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v `pwd`:/fgci-ansible:rw local/c7-systemd /bin/bash -c "/fgci-ansible/tests/test-in-docker-image.sh ${OS_TYPE} ${OS_VERSION} ${ANSIBLE_VERSION}"
