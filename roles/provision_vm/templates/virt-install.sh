ba79c2af736ef34960a94cd3226ec8f0e019787e
Luís Alves <luis.alves@csc.fi>
2015-11-26 13:20:44 +0200

    closes #16
     - removes ansible-role-fgci-admin role
     - creates virt storage pool
     - removes setting inventory_hostname
     - dont set root_pssword
     - create virt pool once with run_once

--- roles/provision_vm/templates/virt-install.sh
+++ roles/provision_vm/templates/virt-install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [ ! -f /etc/libvirt/qemu/{{ inventory_hostname }}.xml ]; then
+if [ ! -f /etc/libvirt/qemu/{{ ansible_hostname }}.xml ]; then
 
 virt-install \
-  --name {{ inventory_hostname }} \
+  --name {{ ansible_hostname }} \
   --os-variant rhel7 \
   --cpu host-model \
   --vcpus {{ vcpus }} \
   --ram {{ ram }} \
 {% for disk in disks %}
-  --disk path={{ disk.path }},cache=writethrough,device=disk,bus=virtio,size={{ disk.size }},format=raw \
+  --disk pool={{ disk.pool }},cache=writethrough,device=disk,bus=virtio,size={{ disk.size }},format=raw \
 {% endfor %}
 {% for bridge in bridges %}
   --network bridge={{ bridge }},model=virtio \
@@ -18,7 +18,7 @@ virt-install \
   --location {{ location }} \
   --graphics=vnc,keymap="fi" \
   --wait 20 \
-  --initrd-inject '{{ kickstart_tempdir }}/{{ inventory_hostname }}.ks' \
-  --extra-args 'ks=file:/{{ inventory_hostname }}.ks console=ttyS0,115200n8' \
+  --initrd-inject '{{ kickstart_tempdir }}/{{ ansible_hostname }}.ks' \
+  --extra-args 'ks=file:/{{ ansible_hostname }}.ks console=ttyS0,115200n8' \
   --console pty,target_type=serial
 fi
