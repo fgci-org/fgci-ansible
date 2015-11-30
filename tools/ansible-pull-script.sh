3c19866ddeadd98da54bced4d56f66c9a4ec819c
Johan Guldmyr <johan.guldmyr@csc.fi>
2015-11-30 15:25:35 +0200

    tools/ansible-pull-script.sh - used to run ansible-pull on a compute

---
+++ tools/ansible-pull-script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# setup the ansible-pull fgci work dir
+mkdir -p $HOME/.ansible/pull/$HOSTNAME
+cd $HOME/.ansible/pull/$HOSTNAME
+if [ ! -d ".git" ]; then
+git clone https://github.com/CSC-IT-Center-for-Science/fgci-ansible.git .
+else
+git pull
+fi
+# install all the dependencies
+ansible-galaxy install -r requirements.yml -f -i
+
+# use -s to add a random sleep
+ansible-pull -U https://github.com/CSC-IT-Center-for-Science/fgci-ansible.git -C ansible-pull -i localhosts
+
