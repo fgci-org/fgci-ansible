d9a2607f247731c541802cd674d541d12d996c49
Johan Guldmyr <johan.guldmyr@csc.fi>
2015-12-02 10:02:38 +0200

    new variable for compute group: ansible_pull_kickstart
     - if true then run curl tools/ansible-pull-script.sh from this repo
     - then run the script

--- tools/ansible-pull-script.sh
+++ tools/ansible-pull-script.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+# This is a script to run ansible-pull on an FGCI compute node
+# It is grabbed with curl during kickstart
 
-# setup the ansible-pull fgci work dir
+# Setup the ansible-pull fgci work dir
 mkdir -p $HOME/.ansible/pull/$HOSTNAME
 cd $HOME/.ansible/pull/$HOSTNAME
 if [ ! -d ".git" ]; then
-git clone https://github.com/CSC-IT-Center-for-Science/fgci-ansible.git .
+/usr/bin/git clone https://github.com/CSC-IT-Center-for-Science/fgci-ansible.git .
 else
-git pull
+# If the .git directory exists then only run git pull
+/usr/bin/git pull
 fi
-# install all the dependencies
-ansible-galaxy install -r requirements.yml -f -i
+# Install all the ansible role dependencies
+/usr/bin/ansible-galaxy install -r requirements.yml -f -i
 
-# use -s to add a random sleep
-ansible-pull -U https://github.com/CSC-IT-Center-for-Science/fgci-ansible.git -C ansible-pull -i /root/hosts
+# Get the hosts file 
+/usr/bin/ansible -m get_url -a "url=http://{{ kickstart_server_ip }}/hosts dest=/root/ mode=0640 owner=root group={{ admingroup }}" localhost
+
+# 10s random delay, fgci-ansible/local.yml playbook, master branch and /root/hosts inventory file
+/usr/bin/ansible-pull -s 10 -U https://github.com/CSC-IT-Center-for-Science/fgci-ansible.git -C master -i /root/hosts
