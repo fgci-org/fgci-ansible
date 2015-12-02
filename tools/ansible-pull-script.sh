7dce399105f7083b36888fb48015eccbe3b1de9c
Johan Guldmyr <johan.guldmyr@csc.fi>
2015-12-02 10:35:43 +0200

    don't set group ownerships - there is on admin group at this stage

--- tools/ansible-pull-script.sh
+++ tools/ansible-pull-script.sh
@@ -15,7 +15,7 @@ fi
 /usr/bin/ansible-galaxy install -r requirements.yml -f -i
 
 # Get the hosts file 
-/usr/bin/ansible -m get_url -a "url=http://{{ kickstart_server_ip }}/hosts dest=/root/ mode=0640 owner=root group={{ admingroup }}" localhost
+/usr/bin/ansible -m get_url -a "url=http://{{ kickstart_server_ip }}/hosts dest=/root/ mode=0644" localhost
 
 # 10s random delay, fgci-ansible/local.yml playbook, master branch and /root/hosts inventory file
 /usr/bin/ansible-pull -s 10 -U https://github.com/CSC-IT-Center-for-Science/fgci-ansible.git -C master -i /root/hosts
