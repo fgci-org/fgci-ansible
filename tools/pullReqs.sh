bb1244654a32033c4429d2c095c841f7530bcd5a
Johan Guldmyr <johan.guldmyr@csc.fi>
2016-07-12 13:42:00 +0300

    pullReqs.sh: also send a flowdock notification

--- tools/pullReqs.sh
+++ tools/pullReqs.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 #
-#
 
+# Install all the ansible roles that we depend on
 /usr/bin/ansible-galaxy install -r requirements.yml --force
 
-# Update requirements_mirror.yml for ansible-pull-script
-ansible-playbook install.yml --tags=fgci-install --diff
+# Update requirements_mirror.yml for ansible-pull-script 
+# sync group_ and host_vars to the install node
+# update ansible-pull-script on the install node
+# send a flowdock notification to fgci admins
+
+ansible-playbook install.yml --tags=fgci-install,flowdock --diff -e flowdock_from_address=pullReqs@{{siteDomain}}
